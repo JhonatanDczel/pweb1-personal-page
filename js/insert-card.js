a1d0af5fe887d9662ef878cea46ac7e5763fad4c
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-14 09:58:19 -0500

    Agrega cards a proyectos

--- js/insert-card.js
+++ js/insert-card.js
@@ -1,4 +1,8 @@
-function createCard(name, link, descriptionProject, img) {
+function createCard(card) {
+  let name = card.name;
+  let link = card.link;
+  let descriptionProject = card.description;
+  let img = card.img;
   var newCard = document.createElement("div");
   newCard.classList.add("project-card");
 
@@ -37,7 +41,37 @@ function createCard(name, link, descriptionProject, img) {
   projectsContainer.appendChild(newCard);
 }
 
-createCard("Calculator", "https://github.com/JhonatanDczel/calculator?tab=readme-ov-file", "Calculadora hecha para poner en práctica mis conocimientos principiantes de JavaScript, como parte de mi aprendizaje autodidacta.", "calculator");
-createCard("Calculator", "#", "Some description", "calculator");
-createCard("Calculator", "#", "Some description", "calculator");
+const sorting = {
+  name: "Sorting Algorithms",
+  link: "https://github.com/JhonatanDczel/FP2-sorting-and-search-pr-01",
+  img: "sorting",
+  description: "Proyecto para el curso Fundamentos de Programacion II, hace una comparación de rendimientos entre algoritmos de ordenamiento y búsqueda. Se usó Java, GNU Plot y bash."
+};
+
+const library = {
+  name: "Library App",
+  link: "https://github.com/JhonatanDczel/library-app",
+  img: "library-app",
+  description: "Una pagina que gestiona los libros que leiste o que leerás. Hecha para practicar la programación orientada a objetos en JavaScript, como parte de mi aprendizaje autodidacta."
+};
+
+const admin = {
+  name: "Admin Dashboard",
+  link: "https://github.com/JhonatanDczel/admin-dashboard",
+  img: "admin-dashboard",
+  description: "Interfaz de un administrador de entradas. Hecho para practicar CSS grid."
+};
+
+const calculator = {
+  name: "Calculator",
+  link: "https://github.com/JhonatanDczel/calculator?tab=readme-ov-file",
+  img: "calculator",
+  description: "Calculadora hecha para poner en práctica el manejo del DOM en JavaScript, como parte de mi aprendizaje autodidacta."
+};
+
+
+createCard(calculator);
+createCard(sorting);
+createCard(library);
+createCard(admin);
 
